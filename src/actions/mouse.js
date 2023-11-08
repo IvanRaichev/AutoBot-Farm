13e838fd2069f16832f96dc533e4f2bbfd31e9e2
IvanRaichev <lalkarus51@gmail.com>
2023-11-08 20:38:03 +0200

    transfer to module

--- src/actions/mouse.js
+++ src/actions/mouse.js
@@ -1,14 +1,41 @@
-function clickTest(h1){
+function findAndClickImage(imagePath) {
+   const screen = robot.screen.capture();
+   const template = cv.imread(imagePath);
+ 
+   const matches = screen.matchTemplate(template, 0.2);
+   const bestMatch = matches.minMaxLoc();
+ 
+   if (bestMatch.maxVal > 0.8) {
+     const { x, y } = bestMatch.maxLoc;
+     const centerX = x + template.cols / 2;
+     const centerY = y + template.rows / 2;
+ 
+     robot.moveMouse(centerX, centerY);
+     robot.mouseClick();
+   }
+ }
+
+ async function findImageOnScreen() {
+   const imagePath = './resources/images/search/find.PNG';
+
+   const screenCapture = robot.screen.capture();
+
+   const targetImage = PNG.sync.read(fs.readFileSync(imagePath));
+
+   const screenImage = new PNG({ width: screenCapture.width, height: screenCapture.height });
+   screenImage.data = screenCapture.image;
+
    
-// const robot = require("robotjs");
+   const diffImage = new PNG({ width: screenCapture.width, height: screenCapture.height });
 
-// robot.moveMouse(100, 100);
+   const numDiffPixels = pixelmatch(screenImage.data, targetImage.data, diffImage.data, screenCapture.width, screenCapture.height, {
+      threshold: 0.1, 
+   });
 
-// robot.mouseClick();
-
-   h1.innerHTML = 1;
+   if (numDiffPixels < 1000) { 
+      console.log('Изображение найдено на экране.');
+   
+   } else {
+      console.log('Изображение не найдено на экране.');
+   }
 }
-
-module.exports = {
-   clickTest,
-};
