35433d780451c6be7d3cb869825a360acb4369b4
IvanRaichev <lalkarus51@gmail.com>
2023-11-12 19:45:47 +0200

    add optimization find Function

--- src/actions/mouse.js
+++ src/actions/mouse.js
@@ -3,18 +3,35 @@ function startBot(robot) {
 
    while (true) {
 
-      msleep(1000);
+      msleep(2000);
 
-      let gate = findGate(robot);
+      let gate = findElement(robot,
+         {
+            x: 1080,
+            y: 432,
+            width: 1110 - 1080,
+            height: 450 - 432
+         },
+         ['54f7f7', '688552', '7f9c5c', '739855']);
 
-      if (gate == false) {
-         console.log('Not found Gate');
+      if (gate != false) {
+         mouseUse(robot, gate.x, gate.y);
       }
 
+      msleep(5000);
 
-      robot.moveMouse(gate.x, gate.y);
-      robot.mouseClick();
+      let duel = findElement(robot,
+         {
+            x: 925,
+            y: 900,
+            width: 1000 - 920,
+            height: 950 - 900
+         },
+         ['cbca32', 'eee132', 'dedc36', 'f6f039']);
 
+      if (duel != false) {
+         mouseUse(robot, duel.x, duel.y);
+      }
    }
 }
 
@@ -22,38 +39,29 @@ function msleep(n) {
    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
-function findGate(robot) {
-   let captureGate = {
-      x: 1080,
-      y: 432,
-      width: 1110 - 1080,
-      height: 450 - 432
-   };
+function mouseUse(robot, x, y) {
 
-   let img = robot.screen.capture(captureGate.x, captureGate.y, captureGate.width, captureGate.height);
-   let gateColor = ['54f7f7', '688552', '7f9c5c', '739855'];
+   robot.moveMouse(x, y);
+   robot.mouseClick();
+}
+
+function findElement(robot, capture, colors) {
+   let img = robot.screen.capture(capture.x, capture.y, capture.width, capture.height);
 
    for (let i = 0; i < img.width; i++) {
       for (let j = 0; j < img.height; j++) {
          let sampleColor = img.colorAt(i, j);
 
-         if (gateColor.includes(sampleColor)) {
-            let screenX = i + captureArea.x;
-            let screenY = j + captureArea.y;
+         if (colors.includes(sampleColor)) {
+            let screenX = i + capture.x;
+            let screenY = j + capture.y;
 
-            console.log("Found a gate at: " + screenX + ", " + screenY);
             return { x: screenX, y: screenY };
          }
       }
    }
 
    return false;
-}
-
-function getRandomInt(min, max) {
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = {
