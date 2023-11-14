525aba9714abbc424467bb3a366089fef78c43d6
IvanRaichev <lalkarus51@gmail.com>
2023-11-14 21:50:03 +0200

    add function startDuel v1

--- src/actions/mouse.js
+++ src/actions/mouse.js
@@ -1,47 +1,53 @@
 async function startBot(robot) {
-   while (true) {
-      let gate = await findElementWithRetry(robot,
-         {
-            x: 1080,
-            y: 432,
-            width: 1110 - 1080,
-            height: 450 - 432
-         },
-         ['54f7f7', '688552', '7f9c5c', '739855']);
+   // while (true) {
+   //    let gate = await findElementWithRetry(robot,
+   //       {
+   //          x: 1080,
+   //          y: 432,
+   //          width: 1110 - 1080,
+   //          height: 450 - 432
+   //       },
+   //       ['54f7f7', '688552', '7f9c5c', '739855']);
 
 
-      if (gate !== false) {
-         console.log('Succes')
-         mouseUse(robot, gate.x, gate.y);
-      }
+   //    if (gate !== false) {
+   //       console.log('Succes')
+   //       mouseUse(robot, gate.x, gate.y);
+   //    }
 
-      let duel = await findElementWithRetry(robot,
-         {
-            x: 925,
-            y: 900,
-            width: 1000 - 920,
-            height: 950 - 900
-         },
-         ['cbca32', 'eee132', 'dedc36', 'f6f039']);
+   //    let duel = await findElementWithRetry(robot,
+   //       {
+   //          x: 925,
+   //          y: 900,
+   //          width: 1000 - 920,
+   //          height: 950 - 900
+   //       },
+   //       ['cbca32', 'eee132', 'dedc36', 'f6f039']);
 
-      msleep(4000);
+   //    msleep(4000);
 
-      if (duel !== false) {
-         console.log('Succes')
-         mouseUse(robot, duel.x, duel.y);
-      }
+   //    if (duel !== false) {
+   //       console.log('Succes')
+   //       mouseUse(robot, duel.x, duel.y);
+   //    }
 
-      msleep(2000);
-      mouseUse(robot, 560, 0);
-      mouseUse(robot, 560, 0);
+   //    msleep(2000);
+   //    mouseUse(robot, 560, 0);
+   //    mouseUse(robot, 560, 0);
 
 
-      msleep(1000);
-      if (duel !== false) {
-         console.log('Succes')
-         mouseUse(robot, duel.x, duel.y);
-      }
-   }
+   //    msleep(1000);
+   //    if (duel !== false) {
+   //       console.log('Succes')
+   //       mouseUse(robot, duel.x, duel.y);
+   //    }
+
+
+   // }
+
+   msleep(1000);
+
+   startDuel(robot);
 }
 
 function msleep(n) {
@@ -84,6 +90,79 @@ function findElement(robot, capture, colors) {
    }
 
    return false;
+}
+
+async function startDuel(robot) {
+
+   const capture = {
+      x: 1090,
+      y: 50,
+      width: 1130 - 1090,
+      height: 76 - 50
+   }
+
+   let player = ['0033bb'];
+   let opponents = ['aa0000'];
+
+   while (true) {
+      let img = robot.screen.capture(capture.x, capture.y, capture.width, capture.height);
+
+      for (let i = 0; i < img.width; i++) {
+         for (let j = 0; j < img.height; j++) {
+            let sampleColor = img.colorAt(i, j);
+
+            if (player.includes(sampleColor)) {
+               await drawCards(robot);
+               let turn = playerTurn(robot);
+               mouseUse(robot, turn.x, turn.y);
+               return false;
+            } else if (opponents.includes(sampleColor)) {
+               console.log('Opponents Turn');
+            }
+         }
+      }
+   }
+}
+
+function playerTurn(robot) {
+   const capture = {
+      x: 715,
+      y: 940,
+      width: 800 - 715,
+      height: 960 - 935
+   }
+
+   let img = robot.screen.capture(capture.x, capture.y, capture.width, capture.height);
+
+   // let cordX = 721;
+   // let cordY = 953;
+
+   let cardChouse = ['42455a'];
+
+   for (let i = 0; i < img.width; i++) {
+      for (let j = 0; j < img.height; j++) {
+         let sampleColor = img.colorAt(i, j);
+
+         if (cardChouse.includes(sampleColor)) {
+            let screenX = i + capture.x;
+            let screenY = j + capture.y;
+
+            return { x: screenX, y: screenY };
+         }
+      }
+   }
+   return false;
+}
+
+async function drawCardAsync(robot) {
+   mouseUse(robot, 560, 0);
+}
+
+async function drawCards(robot) {
+   for (let i = 0; i < 4; i++) {
+      await msleep(500);
+      await drawCardAsync(robot);
+   }
 }
 
 module.exports = {
