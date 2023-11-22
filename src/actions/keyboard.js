a95e7c54be891609bab48df8dc5ee5ff81571d4b
IvanRaichev <lalkarus51@gmail.com>
2023-11-22 17:42:37 +0200

    add function keyboard

--- src/actions/keyboard.js
+++ src/actions/keyboard.js
@@ -1,28 +1,16 @@
-function detectAndPressKey(templateImagePath, searchRegion, scanInterval) {
-   const templateImage = cv.imread(templateImagePath);
- 
-   function scanScreen() {
-     const screen = robot.screen.capture(
-       searchRegion.x,
-       searchRegion.y,
-       searchRegion.width,
-       searchRegion.height
-     );
- 
-     const result = screen.matchTemplate(templateImage, 5);
-     const threshold = 0.8;
-     const matchedPoints = result.minMaxLoc().maxLoc;
- 
-     if (result.confidence >= threshold) {
-       robot.keyTap('a'); // Замените 'a' на нужную вам клавишу
-       console.log('Изображение обнаружено. Клавиша нажата.');
-     } else {
-       console.log('Изображение не обнаружено.');
-     }
-   }
- 
-   // Запуск функции с заданным интервалом
-   setInterval(scanScreen, scanInterval);
- }
 
- module.exports = detectAndPressKey;
+
+function waitForKeypress(key) {
+  const robot = require('robotjs');
+  console.log(`Press ${key} to continue...`);
+
+  const listener = robot.keyTap(key, 'down', () => {
+    robot.removeListener('keydown', listener);
+    console.log(`You pressed ${key}. Program will continue.`);
+  });
+
+  // Ждем завершения программы
+  process.stdin.resume();
+}
+
+ module.exports = waitForKeypress;
