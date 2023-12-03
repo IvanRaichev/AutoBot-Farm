41a5b289ec76c23c728c3d73c728c95d6154de0a
IvanRaichev <lalkarus51@gmail.com>
2023-12-03 20:16:03 +0200

    add config.js

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   mouse: {
+     
+     acceleration: 1.5,
+     smoothing: 2.0,  
+     inverted: false  
+   },
+   keyboard: {
+     delay: 10 
+   },
+   screen: {
+     width: 1920,  
+     height: 1080  
+   }
+ };
