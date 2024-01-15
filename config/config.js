c6b715f7dfdfe330fd6b3d947893cdca07bc8b7a
IvanRaichev <lalkarus51@gmail.com>
2024-01-15 22:51:26 +0200

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,15 +1,18 @@
 module.exports = {
-   mouse: {
-     
-     acceleration: 1.5,
-     smoothing: 2.0,  
-     inverted: false  
-   },
-   keyboard: {
-     delay: 10 
-   },
-   screen: {
-     width: 1920,  
-     height: 1080  
-   }
- };
+  mouse: {
+    acceleration: 1.5,
+    smoothing: 2.0,  
+    inverted: false  
+  },
+  keyboard: {
+    delay: 10 
+  },
+  screen: {
+    width: 1920,  
+    height: 1080  
+  },
+  additionalConfig: {
+    sensitivity: 3.0,
+    customFunctionality: true,
+  }
+};
